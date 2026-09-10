a7fae4048b12b109662cf20aeb6994be7cc3da45
876Mario <github4@dsitservicesja.com>
2026-09-10 10:25:28 -0500

    Update decline.php

--- procurement/decline.php
+++ procurement/decline.php
@@ -1,5 +1,5 @@
 <?php
-$REQUIRE_PERMISSION = 'approve_request';
+$REQUIRE_PERMISSION = 'decline_request';
 require_once $_SERVER['DOCUMENT_ROOT'] . "/config/page_guard.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/config/db.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/config/helper.php";
